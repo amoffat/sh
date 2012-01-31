2623272dad7825517c3c8929c380e9294d05a0c2
Andrew Moffat <andrew.robert.moffat@gmail.com>
2012-01-30 23:07:18 -0600

    fixing version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ except ImportError: from setuptools import setup
 
 setup(
     name="pbs",
-    version="0.4",
+    version="0.5",
     description="Python subprocess wrapper",
     author="Andrew Moffat",
     author_email="andrew.robert.moffat@gmail.com",
