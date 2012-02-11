e9cd892e99a828f82c2219de56a060e99a12e602
Andrew Moffat <andrew.robert.moffat@gmail.com>
2012-02-11 15:45:31 -0600

    bugfix with testing multiple python versions from setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import sys
 import pbs
@@ -7,15 +8,18 @@ except ImportError: from setuptools import setup
 
 
 if sys.argv[1] == "test":
-    versions = ("2.6", "2.7", "3")
+    versions = ("2.6", "2.7", "3", "3.1", "3.2")
     
-    for version in versions:    
-        try: py = pbs.Command("python%s" % version)
-        except pbs.CommandNotFound: pass
-        else:
-            print "Testing Python%s" % version
+    for version in versions:
+        py_version = "python%s" % version
+        py_bin = pbs.which(py_version)
+        if py_bin:
+            py = pbs.Command(py_bin)
+            print("Testing %s" % py_version.capitalize())
             py("test.py", *sys.argv[2:], _fg=True)
-            print
+            print("\n")
+        else:
+            print("Couldn't find %s, skipping" % py_version.capitalize())
     exit(0)
 
 
