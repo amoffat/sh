cf78271d2be7cf89964a5cb04ea1da6354997fe9
Andrew Moffat <andrew.robert.moffat@gmail.com>
2012-03-10 15:31:32 -0600

    backporting unittests from rewrite_popen branch

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 import os
 import sys
 import pbs
+import subprocess
 
 try: from distutils.core import setup
 except ImportError: from setuptools import setup
 
 
-if sys.argv[-1] == "test":
-    os.system("python test.py")
-    sys.exit()
-
+if sys.argv[1] == "test":
+    def run_test(version):
+        py_version = "python%s" % version
+        py_bin = pbs.which(py_version)
+        
+        if py_bin:
+            print("Testing %s" % py_version.capitalize())
+            
+            p = subprocess.Popen([py_bin, "test.py"] + sys.argv[2:])
+            p.wait()
+        else:
+            print("Couldn't find %s, skipping" % py_version.capitalize())
+    
+    versions = ("2.6", "2.7", "3", "3.1", "3.2")
+    
+    for version in versions:
+        run_test(version)
+        
+    exit(0)
+    
 
 setup(
     name="pbs",
