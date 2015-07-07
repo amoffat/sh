698232a018d4f0841a2835c9dcf2725617664f0d
Omer Katz <omer.drow@gmail.com>
2015-07-07 12:48:57 +0300

    Added classifiers to setup.py that indicates that both PyPy and CPython are supported.

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Build Tools",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
