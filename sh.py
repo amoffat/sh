59636d430550a13f61884e5a786bcbac52cf31a3
Andrew Moffat <arwmoffat@gmail.com>
2020-04-26 21:37:00 -0700

    version bump to 1.13.0

--- sh.py
+++ sh.py
@@ -24,7 +24,7 @@ http://amoffat.github.io/sh/
 #===============================================================================
 
 
-__version__ = "1.12.14"
+__version__ = "1.13.0"
 __project_url__ = "https://github.com/amoffat/sh"
 
 
