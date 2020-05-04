cb63a694c679fcb403896e1d711bc81e36ef973a
Andrew Moffat <arwmoffat@gmail.com>
2020-05-04 11:51:12 -0700

    comment update

--- sh.py
+++ sh.py
@@ -1581,6 +1581,8 @@ def compile_args(args, kwargs, sep, prefix):
                 processed_args.append(encode(sub_arg))
         elif isinstance(arg, dict):
             processed_args += aggregate_keywords(arg, sep, prefix, raw=True)
+
+        # see https://github.com/amoffat/sh/issues/522
         elif arg is None or arg is False:
             pass
         else:
