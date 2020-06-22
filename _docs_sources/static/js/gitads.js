922a0c6c11b8659d0236ba6657e7954935ba0f19
Andrew Moffat <arwmoffat@gmail.com>
2020-06-21 20:42:12 -0700

    double slash ad images

--- _docs_sources/static/js/gitads.js
+++ _docs_sources/static/js/gitads.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     let loc1 = $(".wy-menu.wy-menu-vertical");
-    let ad1 = '<br /><a href="https://tracking.gitads.io/?campaign=gitads&repo=sh&redirect=gitads.io"><img src="/sh/_static/ads/gitads-300X250.png" /></a>';
+    let ad1 = '<br /><a href="https://tracking.gitads.io/?campaign=gitads&repo=sh&redirect=gitads.io"><img src="//sh/_static/ads/gitads-300X250.png" /></a>';
     loc1.append(ad1);
 
     let loc2 = $("footer");
-    let ad2 = '<br /><a href="https://tracking.gitads.io/?campaign=gitads&repo=sh&redirect=gitads.io"><img src="/sh/_static/ads/gitads-720X300.png" /></a>';
+    let ad2 = '<br /><a href="https://tracking.gitads.io/?campaign=gitads&repo=sh&redirect=gitads.io"><img src="//sh/_static/ads/gitads-720X300.png" /></a>';
     loc2.append(ad2);
 });
