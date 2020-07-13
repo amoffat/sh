dc88690d258e4e332c963f28e01b74a99541ffc6
Andrew Moffat <arwmoffat@gmail.com>
2020-07-13 06:51:48 -0700

    removing gitads link cruft

--- _docs_sources/static/js/gitads.js
+++ _docs_sources/static/js/gitads.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
     //loc1.append(ad1);
 
     let loc2 = $("footer");
-    let ad2 = '<br /><a href="https://tracking.gitads.io/?campaign=gitads&repo=sh&redirect=gitads.io"><img src="https://images.gitads.io/sh" /></a>';
+    let ad2 = '<br /><a href="https://tracking.gitads.io/?repo=sh"><img src="https://images.gitads.io/sh" /></a>';
     loc2.append(ad2);
 });
